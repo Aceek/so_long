8c8baadeae2eaee28e1a1850c562fb3917fa58eb
Ilan LINHARDT <ilinhard@e2r9p17.clusters.42paris.fr>
2022-06-02 08:14:23 +0200

    parsing correction

--- srcs/check_error.c
+++ srcs/check_error.c
@@ -6,7 +6,7 @@
 /*   By: ilinhard <ilinhard@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/25 09:17:15 by ilinhard          #+#    #+#             */
-/*   Updated: 2022/06/02 06:59:22 by ilinhard         ###   ########.fr       */
+/*   Updated: 2022/06/02 08:13:20 by ilinhard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -72,7 +72,7 @@ char	**create_map(int fd)
 	return (map);
 }
 
-int	ft_check_error(int ac, char *path)
+	int	ft_check_error(int ac, char *path)
 {
 	int		fd;
 	char	**map;
--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: ilinhard <ilinhard@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/02 04:20:33 by ilinhard          #+#    #+#             */
-/*   Updated: 2022/06/02 04:26:54 by ilinhard         ###   ########.fr       */
+/*   Updated: 2022/06/02 08:13:44 by ilinhard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 int	main(int ac, char **av)
 {
-	int	error;
+	int		error;
 
 	error = ft_check_error(ac, av[1]);
 	if (error < 0)
